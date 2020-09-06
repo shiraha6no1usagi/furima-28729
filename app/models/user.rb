1040db99ba437d9aefede3dfad9c6ac684e4fb94
shiraha6no1usagi <usagi.o.sigoto@gmail.com>
2020-09-06 17:11:43 +0900

    ユーザー管理機能修正01

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   has_many :items
   has_many :managements
   with_options presence: true do
+    zen = /\A[ぁ-んァ-ン一-龥]/
+    kana = /\A[ァ-ヶー－]+\z/
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
     validates :nickname
     validates :date
-    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :name_read, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :family_name_read, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :name, format: { with: zen, message: 'is invalid. Input full-width characters.' }
+    validates :family_name, format: { with: zen, message: 'is invalid. Input full-width characters.' }
+    validates :name_read, format: { with: kana, message: 'is invalid. Input full-width katakana characters.' }
+    validates :family_name_read, format: { with: kana, message: 'is invalid. Input full-width katakana characters.' }
   end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
